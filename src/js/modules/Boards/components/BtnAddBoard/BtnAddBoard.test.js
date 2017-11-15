a5099d4703807c055e07581d0984013b311eb3d0
Joao Lavoier <jmlavoier@hotmail.com>
2017-11-14 22:26:09 -0200

    Add: test BtnAddBoard

---
+++ src/js/modules/Boards/components/BtnAddBoard/BtnAddBoard.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import BoardForm from 'modules/Boards/components/BtnAddBoard';
+
+test('should component render with default props', () => {
+  
+});
