167d00a31bc6e84246fa6736e650a50f8ee07a4f
Joao Lavoier <jmlavoier@hotmail.com>
2017-11-18 19:16:58 -0200

    Add test: BoardForm onClickName event

--- src/js/modules/Boards/components/BoardForm/BoardForm.test.js
+++ src/js/modules/Boards/components/BoardForm/BoardForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import BoardForm from './BoardForm';
 
 import uid from 'uid';
@@ -24,4 +24,21 @@ test('Should component render input on edit mode', () => {
   />);
 
   expect(wrapper).toMatchSnapshot();
-})
+});
+
+test('Should component calls onClickName prop on click', () => {
+  const onClickName = jest.fn();
+  const id = uid();
+  
+  const wrapper = shallow(<BoardForm
+    editMode={false}
+    id={id}
+    name="Testing"
+    onClickName={onClickName}
+  />);
+  expect(wrapper).toMatchSnapshot();
+
+  wrapper.find('button').simulate('click');
+  expect(onClickName).toHaveBeenCalled();
+  expect(onClickName.mock.calls[0][0]).toEqual(id);
+});
