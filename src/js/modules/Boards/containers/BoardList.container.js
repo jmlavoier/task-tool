11e0c3b20be1ee070c34774e8694c884f1d7760e
Joao Lavoier <jmlavoier@hotmail.com>
2017-11-19 18:35:05 -0200

    Add test: BoardList

--- src/js/modules/Boards/containers/BoardList.container.js
+++ src/js/modules/Boards/containers/BoardList.container.js
@@ -4,10 +4,12 @@ import { connect } from 'react-redux';
 import { createBoard, saveBoard, editBoard } from 'modules/Boards/actions/Boards.actions';
 import BoardsList from 'modules/Boards/components/BoardsList';
 
+/* istanbul ignore next */
 const mapStateToProps = state => ({
   boards: state.boards,
 });
 
+/* istanbul ignore next */
 const mapDispatchToProps = dispatch => ({
   onBtnAddBoardClick: () => dispatch(createBoard(uid())),
   onFieldNameBlur: board => dispatch(saveBoard(board)),
